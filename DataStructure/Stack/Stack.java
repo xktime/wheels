35ee8cb42fb47b38cef11917015f950eb40e055f
xktime <luoliwen0913@foxmail.com>
2019-09-13 14:03:22 +0800

    修改：Stack的异常判定

--- DataStructure/Stack/Stack.java
+++ DataStructure/Stack/Stack.java
@@ -7,12 +7,12 @@ public class Stack {
 	private int point = 0;//栈顶指针
 	private int INITIAL_VALUE = 4;//初始容量大小
 	//容量达到最大容量的百分比之后，进行扩容或缩小
-	private int EXPANSION_PROBABILITY = 80;
-	private int REDUCTION_PROBABILITY = 50;
-	//扩大或缩小当前最大容量的百分比
+	private int EXPANSION_PROBABILITY = 80;//不能大于100，大于100会导致数组无法扩容
+	private int REDUCTION_PROBABILITY = 50;//不能大于REDUCTION_SIZE，否则会导致当前的栈内元素个数多于缩小后的数组大小，引起越界异常
+	//扩大或缩小成当前最大容量的百分比
 	private int EXPANSION_SIZE = 150;
 	private int REDUCTION_SIZE = 70;
-	private int MAX_PROBABILITY = 100;
+	private double MAX_PROBABILITY = 100;//类型为double是为了计算的时候不需要再转换类型
 
 	private Stack(){
 		arr = new int[INITIAL_VALUE];
@@ -31,20 +31,45 @@ public class Stack {
 	}
 
 	public void push (int value) {
+		if (size() > (length() - 1)) {
+			System.out.println("数组已满，EXPANSION_PROBABILITY不能大于100");
+			return;
+		}
 		arr[point++] = value;
-		int expasion = (int)(arr.length * ((double) EXPANSION_PROBABILITY/MAX_PROBABILITY));
-		if (point >= expasion) {
-			int size = (int)(arr.length * ((double) EXPANSION_SIZE/MAX_PROBABILITY));
+		//数组需要扩容的阈值
+		double expansion = length() * (EXPANSION_PROBABILITY/MAX_PROBABILITY);
+		if (size() >= expansion) {
+			int size = (int)(length() * (EXPANSION_SIZE/MAX_PROBABILITY));
 			autoAdjustedSize(size);
 		}
 	}
 
 	public void pull() {
-		int reduction = (int)(arr.length * ((double) REDUCTION_PROBABILITY/MAX_PROBABILITY));
-		if (--point < reduction) {
-			int size = (int)(arr.length * ((double) REDUCTION_SIZE/MAX_PROBABILITY));
+		if (size() == 0) {
+			System.out.println("栈内没有元素，不能执行PULL操作");
+			return;
+		}
+		--point;
+		//数组需要缩小的阈值
+		double reduction = length() * (REDUCTION_PROBABILITY/MAX_PROBABILITY);
+		if (size() < reduction) {
+			int size = (int)(length() * (REDUCTION_SIZE/MAX_PROBABILITY));
 			autoAdjustedSize(size);
 		}
+	}
+
+	/**
+	 * @return 栈内当前元素的个数
+	 */
+	public int size() {
+		return point;
+	}
+
+	/**
+	 * @return 栈的实际大小
+	 */
+	public int length() {
+		return arr.length;
 	}
 
 	public String toString() {
--- Test/DataStructure/StackTest.java
+++ Test/DataStructure/StackTest.java
@@ -6,10 +6,12 @@ public class StackTest {
 		Stack stack = Stack.getMe();
 		for (int i = 0; i < 10; i++) {
 			stack.push(i);
+			System.out.println("栈的元素个数为：" + stack.size() + ";栈的实际大小为：" + stack.length());
 		}
 		System.out.println(stack);
 		for (int i = 0; i <8; i++) {
 			stack.pull();
+			System.out.println("栈的元素个数为：" + stack.size() + ";栈的实际大小为：" + stack.length());
 		}
 		System.out.println(stack);
 	}
