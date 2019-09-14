b561bee0555bec28afaa199ecfa858e2d5bf02b0
xktime <luoliwen0913@foxmail.com>
2019-09-14 14:13:47 +0800

    将Stack单例改为多例

--- DataStructure/Stack/Stack.java
+++ DataStructure/Stack/Stack.java
@@ -1,11 +1,8 @@
 package Stack;
 
-public class Stack {
-	private final static Object lock_object = new Object();//加锁用的对象
-	private static Stack stack;
-	private int[] arr;
+public class Stack<T> {
+	private T[] arr;
 	private int point = 0;//栈顶指针
-	private int INITIAL_VALUE = 4;//初始容量大小
 	//容量达到最大容量的百分比之后，进行扩容或缩小
 	private int EXPANSION_PROBABILITY = 80;//不能大于100，大于100会导致数组无法扩容
 	private int REDUCTION_PROBABILITY = 50;//不能大于REDUCTION_SIZE，否则会导致当前的栈内元素个数多于缩小后的数组大小，引起越界异常
@@ -14,23 +11,11 @@ public class Stack {
 	private int REDUCTION_SIZE = 70;
 	private double MAX_PROBABILITY = 100;//类型为double是为了计算的时候不需要再转换类型
 
-	private Stack() {
-		arr = new int[INITIAL_VALUE];
+	public Stack(int size) {
+		arr = (T[])new Object[size];
 	}
 
-	//单例，使用单例是方便以后用来测试多线程
-	public static Stack getMe() {
-		if (stack == null) {
-			synchronized (lock_object) {
-				if (stack == null) {
-					stack = new Stack();
-				}
-			}
-		}
-		return stack;
-	}
-
-	public void push(int value) {
+	public void push(T value) {
 		if (size() > (length() - 1)) {
 			System.out.println("数组已满，EXPANSION_PROBABILITY不能大于100");
 			return;
@@ -40,22 +25,23 @@ public class Stack {
 		double expansion = length() * (EXPANSION_PROBABILITY / MAX_PROBABILITY);
 		if (size() >= expansion) {
 			int size = (int) (length() * (EXPANSION_SIZE / MAX_PROBABILITY));
-			autoAdjustedSize(size);
+			resize(size);
 		}
 	}
 
-	public void pull() {
+	public T pull() {
 		if (size() == 0) {
 			System.out.println("栈内没有元素，不能执行PULL操作");
-			return;
+			return null;
 		}
-		--point;
+		T value = arr[--point];
 		//数组需要缩小的阈值
 		double reduction = length() * (REDUCTION_PROBABILITY / MAX_PROBABILITY);
 		if (size() < reduction) {
 			int size = (int) (length() * (REDUCTION_SIZE / MAX_PROBABILITY));
-			autoAdjustedSize(size);
+			resize(size);
 		}
+		return value;
 	}
 
 	/**
@@ -91,12 +77,12 @@ public class Stack {
 	 *
 	 * @param size 调整后数组的大小
 	 */
-	private void autoAdjustedSize(int size) {
+	private void resize(int size) {
 		if (point > size) {
 			System.out.println("调整数组大小失败，REDUCTION_SIZE不能小于REDUCTION_PROBABILITY");
 			return;
 		}
-		int[] tempArr = new int[size];
+		T[] tempArr = (T[])new Object[size];
 		for (int i = 0; i < point; i++) {
 			tempArr[i] = arr[i];
 		}
--- Test/DataStructure/StackTest.java
+++ Test/DataStructure/StackTest.java
@@ -3,14 +3,14 @@ import Stack.Stack;
 
 public class StackTest {
 	public static void main(String[] args) {
-		Stack stack = Stack.getMe();
+		Stack<Integer> stack = new Stack(4);
 		for (int i = 0; i < 10; i++) {
 			stack.push(i);
 			System.out.println("栈的元素个数为：" + stack.size() + ";栈的实际大小为：" + stack.length());
 		}
 		System.out.println(stack);
 		for (int i = 0; i <8; i++) {
-			stack.pull();
+			System.out.println(stack.pull() + "被pull了");
 			System.out.println("栈的元素个数为：" + stack.size() + ";栈的实际大小为：" + stack.length());
 		}
 		System.out.println(stack);
