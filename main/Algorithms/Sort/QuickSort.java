ad0f1b786c42cad8d9ee0fc216b3dda4cce0df64
xktime <luoliwen0913@foxmail.com>
2019-11-15 14:45:11 +0800

    修改：快速排序

--- main/Algorithms/Sort/QuickSort.java
+++ main/Algorithms/Sort/QuickSort.java
@@ -3,17 +3,16 @@ package Algorithms.Sort;
 import Util.Collection.List.List;
 import Util.Funciton.Collections;
 
+/**
+ * 快速排序
+ */
 public class QuickSort implements Sort {
     @Override
     public void sort(List<? extends Comparable> list) {
         Collections.shuffle(list);//打乱，
-//        quickSort(list, 0, list.size() - 1); //普通快速排序
-        quick3way(list, 0, list.size() - 1);//3向快速排序
+        quickSort(list, 0, list.size() - 1); //普通快速排序
     }
 
-    /**
-     * 快速排序
-     */
     private void quickSort(List<? extends Comparable> list, int low, int high) {
         if (list == null || list.isEmpty()) {
             return;
@@ -50,37 +49,9 @@ public class QuickSort implements Sort {
             if (lo >= partition) {
                 break;
             }
-            Collections.swap(list, lo, partition);
+            Collections.swap(list, lo++, partition--);
         }
         Collections.swap(list, low, partition);
         return partition;
-    }
-
-    /**
-     * 3向快速排序（适用于相同元素较多的情况）
-     */
-    private void quick3way(List<? extends Comparable> list, int low, int high) {
-        if (list == null || list.isEmpty()) {
-            return;
-        }
-        if (low >= high) {
-            return;
-        }
-        int lo = low;//list[low..lo]都小于基准元素
-        int mid = low + 1;//list[lo..mid]都等于基准元素
-        int hi = high;//list[hi..high]都大于基准元素
-        //基准元素
-        Comparable c = list.get(low);
-        while(mid <= hi) {
-            if (c.compareTo(list.get(mid)) > 0) {
-                Collections.swap(list, lo++, mid++);
-            } else if (c.compareTo(list.get(mid)) < 0) {
-                Collections.swap(list, mid, hi--);
-            } else {
-                mid++;
-            }
-        }
-        quick3way(list, low, lo);//将小于基准元素的list排序
-        quick3way(list, mid, high);//将大于基准元素的list排序
     }
 }
