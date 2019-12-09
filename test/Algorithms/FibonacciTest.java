85283aed21fad95cc41943c45275a21b14d7dd08
xktime <luoliwen0913@foxmail.com>
2019-12-09 21:38:08 +0800

    修改：Fibonacci

--- test/Algorithms/FibonacciTest.java
+++ test/Algorithms/FibonacciTest.java
@@ -4,11 +4,11 @@ public class FibonacciTest {
     public static void main(String[] args) {
         System.out.println("=============递归===================");
         for (int i = 1; i < 10; i++) {
-            System.out.println("第" + i + "位Fibonacci数为：" +new Fibonacci().byRecursive(i));
+            System.out.println("第" + i + "位Fibonacci数为：" + new Fibonacci().byRecursive(i));
         }
         System.out.println("=============循环===================");
         for (int i = 1; i < 10; i++) {
-            System.out.println("第" + i + "位Fibonacci数为：" +new Fibonacci().byCircle(i));
+            System.out.println("第" + i + "位Fibonacci数为：" + new Fibonacci().byCircle(i));
         }
     }
 }
