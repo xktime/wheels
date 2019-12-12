a2645fcdd5b204ce6de7123fd2309fa971a47b3a
xktime <luoliwen0913@foxmail.com>
2019-12-12 21:17:44 +0800

    LeetCodeNo.89格雷编码

---
+++ main/LeetCode/GrayCode.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import Util.Collection.List.ArrayListImpl;
+import Util.Collection.List.List;
+
+/**
+ * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+ * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。格雷编码序列必须以 0 开头。
+ */
+public class GrayCode {
+    public static void main(String[] args) {
+        System.out.println(new GrayCode().grayCode(2));
+    }
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> arr = new ArrayListImpl<>();
+        for (int i = 0; i < 1 << n; ++i)
+            arr.add(i ^ i >> 1);
+        return arr;
+    }
+}
